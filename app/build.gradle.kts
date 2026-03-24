c5d2b1236129e29a28b0a71f3764fd8a43f6751e
yaqinoel <yaqizhang.unique@outlook.com>
2026-03-24 14:21:25 +0100

    Added dependencies for the projet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,11 +46,27 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    // firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-storage")
+
+    // navigation compose
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    // image loading
+    implementation("io.coil-kt:coil-compose:2.6.0")
+    // icon extends
+    implementation("androidx.compose.material:material-icons-extended")
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
