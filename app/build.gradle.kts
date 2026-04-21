56a813006f98b3c8e2538301d8d03d54180c3279
yaqinoel <yaqizhang.unique@outlook.com>
2026-04-21 14:05:31 +0200

    Cacher the key for google map service

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     id("com.google.gms.google-services")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
