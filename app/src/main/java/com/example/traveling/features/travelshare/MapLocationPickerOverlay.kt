dc9f74fadeda4c98a8057ff215dfc0fdecd1722f
yaqinoel <yaqizhang.unique@outlook.com>
2026-05-15 16:09:16 +0200

    Fix a bug

--- app/src/main/java/com/example/traveling/features/travelshare/MapLocationPickerOverlay.kt
+++ app/src/main/java/com/example/traveling/features/travelshare/MapLocationPickerOverlay.kt
@@ -2,6 +2,8 @@ package com.example.traveling.features.travelshare
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
